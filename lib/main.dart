0c146a05a7dc4a549533f16c0bca9ec10c9a3b8c
Ales Zima <ales@cpex.cz>
2023-11-29 19:59:38 +0100

    Setup main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:dronetag_planner/screens/login_screen.dart';
+
+final _theme = ThemeData.light().copyWith(
+  colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF0CA7F4)),
+  textTheme: GoogleFonts.titilliumWebTextTheme(),
+);
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +18,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      home: const LoginScreen(),
+      theme: _theme,
     );
   }
 }
