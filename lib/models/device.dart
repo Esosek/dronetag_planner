4fcc20d72d3ed44ae416afc0e053cfc214840842
Ales Zima <ales@cpex.cz>
2023-11-29 16:33:07 +0100

    Device data model

---
+++ lib/models/device.dart
@@ -0,0 +1,19 @@
+class Device {
+  const Device({
+    required this.uasId,
+    this.label = '',
+    this.isActive = false,
+  });
+
+  Device setActive(bool value) {
+    return Device(
+      uasId: uasId,
+      label: label,
+      isActive: value,
+    );
+  }
+
+  final String uasId;
+  final String label;
+  final bool isActive;
+}
