d72a6ab7f9389ee67ce23389d96719895913cdb9
Ales Zima <ales@cpex.cz>
2023-11-29 16:38:29 +0100

    FlightLocation data model

---
+++ lib/models/flight_location.dart
@@ -0,0 +1,11 @@
+class FlightLocation {
+  const FlightLocation({
+    required this.latitude,
+    required this.longitude,
+    required this.radius,
+  });
+
+  final double latitude;
+  final double longitude;
+  final int radius;
+}
