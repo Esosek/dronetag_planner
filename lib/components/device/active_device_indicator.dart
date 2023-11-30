74a0e911fddab241c7b7de629c271a25a13e92ff
Ales Zima <ales@cpex.cz>
2023-11-30 13:01:24 +0100

    Added ActiveDeviceIndicator

--- lib/components/device/active_device_indicator.dart
+++ lib/components/device/active_device_indicator.dart
@@ -1,27 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ActiveDeviceIndicator extends StatelessWidget {
+import 'package:dronetag_planner/providers/devices_provider.dart';
+import 'package:dronetag_planner/components/ui/custom_elevated_button.dart';
+
+class ActiveDeviceIndicator extends ConsumerWidget {
   const ActiveDeviceIndicator({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final devices = ref.watch(devicesProvider);
+    final activeDevice = devices.firstWhere((device) => device.isActive);
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        CircleAvatar(
-          radius: 40,
-          child: SizedBox(
-            width: 70,
-            child: Text(
-              'Millennium Falcon',
-              maxLines: 2,
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.bodySmall!.copyWith(),
-            ),
-          ),
-        ),
+        CustomElevatedButton(
+            label: activeDevice.label ?? activeDevice.uasId,
+            foregroundColor: Theme.of(context).colorScheme.onSecondaryContainer,
+            backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+            fixWidth: 160,
+            onPressed: () {}),
         Text(
-          'Change device',
-          style: Theme.of(context).textTheme.labelMedium,
+          'Tap to switch',
+          style: Theme.of(context)
+              .textTheme
+              .labelSmall!
+              .copyWith(letterSpacing: 1),
         ),
       ],
     );
--- lib/components/ui/custom_elevated_button.dart
+++ lib/components/ui/custom_elevated_button.dart
@@ -1,21 +1,35 @@
 import 'package:flutter/material.dart';
 
 class CustomElevatedButton extends StatelessWidget {
-  const CustomElevatedButton(
-      {super.key, required this.label, required this.onPressed});
+  const CustomElevatedButton({
+    super.key,
+    required this.label,
+    required this.onPressed,
+    this.backgroundColor,
+    this.foregroundColor,
+    this.fixWidth,
+  });
 
   final String label;
   final Function() onPressed;
+  final Color? backgroundColor;
+  final Color? foregroundColor;
+  final double? fixWidth;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
-        minimumSize: const Size(90, 40),
+    return SizedBox(
+      width: fixWidth,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          foregroundColor: foregroundColor,
+          backgroundColor:
+              backgroundColor ?? Theme.of(context).colorScheme.primaryContainer,
+          minimumSize: const Size(90, 40),
+        ),
+        onPressed: onPressed,
+        child: Text(label),
       ),
-      onPressed: onPressed,
-      child: Text(label),
     );
   }
 }
