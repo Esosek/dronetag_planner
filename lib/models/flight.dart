c6187fb81c0daba2e259b47b45f1591e01bbaa26
Ales Zima <ales@cpex.cz>
2023-11-30 09:24:53 +0100

    Flight getFormattedDate

--- lib/models/flight.dart
+++ lib/models/flight.dart
@@ -1,17 +1,36 @@
+import 'package:dronetag_planner/models/device.dart';
 import 'package:dronetag_planner/models/flight_location.dart';
 
 class Flight {
   const Flight({
-    required this.uasId,
+    required this.device,
     required this.location,
     required this.altitudeRange,
     required this.dateStart,
     required this.dateEnd,
   });
 
-  final String uasId;
+  final Device device;
   final FlightLocation location;
   final List<int> altitudeRange;
   final String dateStart;
   final String dateEnd;
+
+  String get formattedDate {
+    String formattedStart = formatDateTime(dateStart);
+    String formattedEnd = formatDateTime(dateEnd, includeDate: false);
+    return '$formattedStart - $formattedEnd';
+  }
+
+  String formatDateTime(String dateTimeString, {bool includeDate = true}) {
+    DateTime dateTime = DateTime.parse(dateTimeString);
+    String formattedTime =
+        '${dateTime.hour.toString().padLeft(2, '0')}:${dateTime.minute.toString().padLeft(2, '0')}';
+
+    if (includeDate) {
+      return '${dateTime.day.toString().padLeft(2, '0')}.${dateTime.month.toString().padLeft(2, '0')}.${dateTime.year}, $formattedTime';
+    } else {
+      return formattedTime;
+    }
+  }
 }
